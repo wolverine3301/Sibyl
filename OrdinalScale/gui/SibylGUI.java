a189bc4b5074b83147127dd49be13b5a317dd25d
Cade <Cade@10.0.0.79>
2019-08-05 14:16:02 -0700

    Updated GUI's action listeners.

--- OrdinalScale/gui/SibylGUI.java
+++ OrdinalScale/gui/SibylGUI.java
@@ -1,12 +1,18 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.TreeSet;
 
+import javax.swing.AbstractAction;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
@@ -29,21 +35,26 @@ public class SibylGUI extends JFrame{
     /** The JPanel which holds the options of successes and defaults option. */
     private JPanel sdOptionsMenu;
     
+    /** Holds the inputs for the successes and defaults. Index 1 = successes, index 0 = defaults */
+    private String[] sdInputs;
+    
     /** Tool bar with options for each column. */
     private JToolBar genericOptionsMenu;
     
     /** Holds the input values for a given column */
     private HashMap<String, String> variableInputs;
     
+    
     /**
      * Constructs a new instance of the GUI.
-     * @param theCSV the CSV file to pass the GUI.
+     * @param theDataFrame reference to the main data frame.
      */
     public SibylGUI(DataFrame<String> theDataFrame) {
         super(GUI_NAME);
         dataFrame = theDataFrame;
         columnsToUse = dataFrame.getColumnNames();
         sdOptionsMenu = new JPanel(new BorderLayout());
+        sdInputs = new String[2];
         genericOptionsMenu = new JToolBar("Options");
         variableInputs = new HashMap<String, String>();
     }
@@ -74,17 +85,40 @@ public class SibylGUI extends JFrame{
                 JTextField textInput = new JTextField(10);
                 currentPanel.add(currentLabel, BorderLayout.NORTH);
                 currentPanel.add(textInput, BorderLayout.SOUTH);
+                textInput.addActionListener(new InputActionListener(columnsToUse[i], true));
+                variableInputs.put(columnsToUse[i], null);
             } else {                            //DROP DOWN MENU
                 JComboBox<String> dropDownInput = new JComboBox<String>(currentOptions.toArray(new String[0]));
                 currentPanel.add(currentLabel, BorderLayout.NORTH);
                 currentPanel.add(dropDownInput, BorderLayout.SOUTH);
+                variableInputs.put(columnsToUse[i], (String) dropDownInput.getSelectedItem());
+                dropDownInput.addActionListener(new InputActionListener(columnsToUse[i], false));
             }
             genericOptionsMenu.add(currentPanel);
             genericOptionsMenu.addSeparator();
-            // TO DO : STORE INPUTS
         }
+        JButton runButton = new JButton("Run Algorithm");
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                runAlgorithm();
+            }   
+        });
+        genericOptionsMenu.add(runButton);
     }
     
+    /**
+     * Runs the algorithm.
+     */
+    private void runAlgorithm() {
+        if (variableInputs.containsValue(null) || sdInputs[0] == null || sdInputs[1] == null) {
+            JOptionPane.showMessageDialog(this, "One or more inputs have not been properly initialized.", 
+                    "Invalid Input(s)",
+                    JOptionPane.ERROR_MESSAGE);
+        } else {
+            System.out.println(variableInputs); //temporary to check for proper storage of inputs.
+        }
+    }
     /**
      * Builds the proportion of defaults and succsesses options menu.
      */
@@ -92,17 +126,75 @@ public class SibylGUI extends JFrame{
         JLabel defaults = new JLabel("Proportion of Defaults", JLabel.TRAILING);
         JPanel defaultPanel = new JPanel(new BorderLayout());
         JTextField dTextBox = new JTextField(10);
+        dTextBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sdInputs[0] = dTextBox.getText();
+            }
+            
+        });
         defaults.setLabelFor(dTextBox);
         defaultPanel.add(dTextBox, BorderLayout.EAST);
         defaultPanel.add(defaults, BorderLayout.WEST);
         JLabel succsesses = new JLabel("Proportion of Succsesses", JLabel.TRAILING);
         JPanel succsessesPanel = new JPanel(new BorderLayout());
         JTextField sTextBox = new JTextField(10);
+        sTextBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sdInputs[1] = sTextBox.getText();
+            }
+            
+        });
         succsesses.setLabelFor(sTextBox);
         succsessesPanel.add(sTextBox, BorderLayout.EAST);
         succsessesPanel.add(succsesses, BorderLayout.WEST);
         sdOptionsMenu.add(defaultPanel, BorderLayout.NORTH);
         sdOptionsMenu.add(succsessesPanel, BorderLayout.SOUTH);
+    }
+    
+    /**
+     * Creates an action listener for a combo box or text box.
+     * @author Cade Reynoldson
+     * @version 1.0
+     */
+    private class InputActionListener extends AbstractAction {
+
+        /** STOP GIVING ME ERRORS FOR NOT HAVING THIS *ANGRY REACT* */
+        private static final long serialVersionUID = 8379111664161837548L;
+        
+        /** Name of the input box */
+        private String inputName;
+        
+        /** Indicates if the action listener is for a textbox. */
+        private boolean isTextBox;
+        
+        /**
+         * Creates an action listener for a combo box.
+         * @param theName the name of the box.
+         * @param textOrCombo true for text box, false for combo box.
+         */
+        public InputActionListener(String theName, boolean textOrCombo) {
+            inputName = theName;
+            isTextBox = textOrCombo;
+            
+        }
+        
+        /** 
+         * Replaces the current value in the variable input map with the newly selected one. 
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (isTextBox) {
+                JTextField tempTB = (JTextField) e.getSource();
+                variableInputs.replace(inputName, tempTB.getText());
+            } else {
+                @SuppressWarnings("unchecked")
+                JComboBox<String> tempCB = (JComboBox<String>) e.getSource();
+                variableInputs.replace(inputName, (String) tempCB.getSelectedItem());
+            }
+        }
+        
     }
 
 }
