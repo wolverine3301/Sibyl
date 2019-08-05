e76945085496b8135ded5a12d74deec5fae42033
Cade <Cade@10.0.0.79>
2019-08-05 12:20:54 -0700

    Added runner for GUI.

---
+++ OrdinalScale/gui/SibylGUIRunner.java
@@ -0,0 +1,34 @@
+package gui;
+
+import saga.DataFrame;
+
+public class SibylGUIRunner {
+    static DataFrame dataFrame;
+    /**
+     * Prevents the class from being instantiated.
+     */
+    private SibylGUIRunner() {
+        throw new IllegalStateException();
+    }
+    
+    /**
+     * Runs the power paint gui.
+     * @param theArgs allows aruments to be passed.
+     */
+    public static void main(final String[] theArgs) {
+//        EventQueue.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                dataFrame = new DataFrame();
+//                String[] types = {"target","numeric","numeric","category","meta","meta"};
+//                dataFrame.loadcsv("testfiles/testing.csv", types);
+//                new SibylGUI2(dataFrame).start();
+//            }
+//        });
+      dataFrame = new DataFrame();
+      String[] types = {"target","numeric","numeric","category","meta","meta"};
+      dataFrame.loadcsv("testfiles/testing.csv", types);
+      new SibylGUI(dataFrame).start();
+    }
+
+}
