ee7931ee1a914113baa45e74b666626c510bd0b4
logan.collier <logancollier04@gmail.com>
2019-08-09 14:08:13 -0500

    update to distance

--- OrdinalScale/clairvoyance/Euclidean.java
+++ OrdinalScale/clairvoyance/Euclidean.java
@@ -4,14 +4,22 @@ import saga.*;
 
 public class Euclidean extends Distance{
 	public double distance(Row r1, Row r2) {
-		double distance;
+		double distance = 0;
 		for(int i = 0;i < r1.getlength();i++) {
 			Particle p1 = r1.getParticle(i);
-			Particle p2 = r2.getParticle(i)
-			if(p1.type.contains("String"))
-			distance = distance + Math.pow((r2.values.get(i) - r1.values.get(i)),2 );
-			
-		}
+			Particle p2 = r2.getParticle(i);
+			//if the column is a string for categorical variablke
+			if(p1.type.contains("Category")) {
+				//if they are the same there is no distance to add
+				if(!p1.type.contentEquals(p2.type)) {
+					distance = distance + 1;
+				}
+			}
+			else {
+				distance = distance + Math.pow(((double)p2.getValue() - (double)p1.getValue()),2 );
+			}
+		}//end for
+		return Math.sqrt(distance);
 		
 	}
 
