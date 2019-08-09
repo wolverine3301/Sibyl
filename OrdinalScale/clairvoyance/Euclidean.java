cc3f37a98d9955d3ac7460e9032803af6c04df96
logan.collier <logan.collier@CASH-1020.cashfairy.local>
2019-08-09 14:00:30 -0500

    euclidian distance method

--- OrdinalScale/clairvoyance/Euclidean.java
+++ OrdinalScale/clairvoyance/Euclidean.java
@@ -6,7 +6,9 @@ public class Euclidean extends Distance{
 	public double distance(Row r1, Row r2) {
 		double distance;
 		for(int i = 0;i < r1.getlength();i++) {
-			if( r2.row.get(i).getType())
+			Particle p1 = r1.getParticle(i);
+			Particle p2 = r2.getParticle(i)
+			if(p1.type.contains("String"))
 			distance = distance + Math.pow((r2.values.get(i) - r1.values.get(i)),2 );
 			
 		}
