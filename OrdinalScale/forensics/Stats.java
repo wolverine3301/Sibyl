7116743032ee291b8ac9c38c44b622c46e445deb
logan.collier <logan.collier@CASH-1020.cashfairy.local>
2019-08-12 17:47:37 -0500

    updated stats class, entropy method still needs work

--- OrdinalScale/forensics/Stats.java
+++ OrdinalScale/forensics/Stats.java
@@ -1,14 +1,41 @@
 package forensics;
+import java.util.HashMap;
+import java.util.Map;
+
 import saga.*;
 
 public class Stats {
-	public static void mean(Column c) {
+	public static double mean(Column c) {
+		if(c.type.contains("Integer") || c.type.contains("Double")) {
+			double sum = 0;
+			// get sum
+			for(int i = 0;i < c.getLength();i++) {
+
+				sum = sum + ((Number) c.getValue(i)).doubleValue();
+			}//end sum
+			return sum / c.getLength();	
+		}else {
+			return 0;
+		}
 		
 	}
-	public static void variance(Column c) {
-		
+	public static double variance(Column c) {
+		double mean = mean(c);
+		double var = 0;
+		for(int i = 0;i < c.getLength();i++) {
+			var = Math.pow((((Number) c.getValue(i)).doubleValue() - mean), 2);
+		}
+		return var/c.getLength();
 	}
-	public static void standardDeviation(Column c) {
+	public static double standardDeviation(Column c) {
+		return Math.sqrt(variance(c));		
+	}
+	public static double entropy(Column c) {
+		HashMap values = c.uniqueValCnt();
+		for (Object value : values.values()) {
+			System.out.println(value);
+		}
+		return 0;
 		
 	}
 
