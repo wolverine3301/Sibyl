0dd5d98ac46c32e821f6fd093e1d43c6fef03927
Gilgamesh69 <logancollier04@gmail.com>
2019-08-20 18:23:11 -0500

    Info gain

--- OrdinalScale/forensics/InformationGain.java
+++ OrdinalScale/forensics/InformationGain.java
@@ -1,7 +1,9 @@
 package forensics;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 import saga.*;
 
@@ -24,18 +26,36 @@ public class InformationGain {
 		targets = df.getColumnByTypes("target");
 	}
 	public double gain(int index) {
-		HashMap<Object, Integer> target_info = targets.get(index).uniqueValCnt();
-		HashMap<Object, Integer> attribute_info;
-		for(Column i : df.columns) {
-			if(i.type == "target") {
-				continue;
-			}
-			else {
-				attribute_info = i.uniqueValCnt();
-				
+		HashMap<Object, List<HashMap<Object,Integer>>> joint_info = new HashMap<Object, List<HashMap<Object,Integer>>>();
+		Set<Object> target_info = targets.get(index).uniqueValues();
+		Set<Object> attribute_info;
+		//Initialize
+		List<HashMap<Object, Integer>> col;
+		HashMap<Object, Integer> vals;
+		for(Object key1 : target_info) {
+			//columns
+			for(Column c : df.columns) {
+				col = new ArrayList<HashMap<Object,Integer>>();
+				attribute_info = c.uniqueValues();
+				//unique vals in column
+				for(Object key2 : attribute_info) {
+					vals = new HashMap<Object,Integer>();
+					vals.put(key2, 0);
+					col.add(vals);
+				}
+				joint_info.put(key1, col);	
 			}
 			
 		}
+		
+		
+	}
+	public double entropy(Integer[] p) {
+		double e = 0;
+		for(int i = 0; i < p.length;i++) {
+			e = e + (-1)* ((double)p[i]/df.getLength()) * (Math.log10((double)p[i]/df.getLength()) / Math.log10(2));
+		}
+		return e;
 		
 	}
 
