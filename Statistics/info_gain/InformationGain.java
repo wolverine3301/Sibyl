70e9bdb664378ec9efa42285dd0ecfe0b3fd78b6
Cade <Cade@10.0.0.127>
2019-10-02 15:17:09 -0700

    Comleted info gain

--- Statistics/info_gain/InformationGain.java
+++ Statistics/info_gain/InformationGain.java
@@ -12,14 +12,27 @@ import dataframe.DataFrame;
 
 import java.util.Set;
 
-
+/**
+ * Computes a modified version of the information gain algorithm. Note: Target columns must be marked before trying to run algorithm.
+ * @author Cade Reynoldson 
+ * @author Logan Collier
+ * @version 1.0
+ */
 public class InformationGain {
-	private DataFrame df;
+	
+    /** The data frame to compute info gain on. */
+    private DataFrame df;
+	
+	/** The target columns. */
 	private List<Column> targets;
 	
+	/** Not used by cade's implentation */
 	public HashMap<String,Double> info_gains;
 	
-	
+	/**
+	 * Creates and initializes all data neccesary for the algorithm.
+	 * @param df the data frame to compute info gain on.
+	 */
 	public InformationGain(DataFrame df) {
 		this.df = df;
 		info_gains = new HashMap<String,Double>();
@@ -27,6 +40,7 @@ public class InformationGain {
 		
 
 	}
+	
 	/**
 	 * set target variables
 	 */
@@ -34,7 +48,13 @@ public class InformationGain {
 		targets = df.getColumnByTypes('T');
 	}
 	
-	public void cadeGain(int index) {
+	/**
+	 * Implementation of information gain. Computes the gain of all categorical variables to the given index of a 
+	 * target in the targets list. 
+	 * @param index the index of the target in the target list.
+	 * @return a "sorted" array list of columns, with the lowest index (0) representing most gain, and highest index (list.size() - 1) representing least gain.
+	 */
+	public ArrayList<Column> infoGain(int index) {
 	    HashMap<Object, Integer> targetInfo = ColumnTools.uniqueValCnt(targets.get(index));
 	    double targetEntropy = entropy(targetInfo);
 	    //Holds the calculated info gain in a max heap style.
@@ -44,52 +64,94 @@ public class InformationGain {
                 return Double.compare(o2.getInfoGain(), o1.getInfoGain());
             }
 	    });
-	    for (int i = 0; i < df.getNumColumns(); i++) {
+	    for (int i = 0; i < df.getNumColumns(); i++) { //Calculate info gain of every column compared to the target column
 	        if (df.getColumn_byIndex(i).getType() == 'C') {
 	            HashMap<Object, Integer> uniqueColumn = ColumnTools.uniqueValCnt(df.getColumn_byIndex(i));
 	            double tempEntropy = entropy(uniqueColumn);
 	            infoGain.add(new EntropyData(i, targetEntropy - tempEntropy));
 	        }
 	    }
-	    System.out.println(infoGain);
+	    ArrayList<Column> sortedGains = new ArrayList<Column>();
+	    while(!infoGain.isEmpty())
+	        sortedGains.add(df.getColumn_byIndex(infoGain.remove().getIndex()));
+	    return sortedGains;
 	}
 	
 	/**
-	 * Used for optimizing entropy calculations.
-	 * @author Cade
-	 *
+	 * Used for optimizing entropy calculations & fetching columns.
+	 * @author Cade Reynoldson
+	 * @version 1.0
 	 */
 	private class EntropyData {
+	    
+	    /** The index of the column in which the entropy was calculated to. */
 	    private int columnIndex;
+	    
+	    /** The information gained */
 	    private double infoGain;
+	    
+	    /**
+	     * Creates a new instance of this (no fucking shit)
+	     * @param theColumnIndex COLUMN INDEX
+	     * @param theInfoGain THE INFO GAIN??!?!?!?!?
+	     */
 	    public EntropyData(int theColumnIndex, double theInfoGain) {
 	        columnIndex = theColumnIndex;
 	        infoGain = theInfoGain;
 	    }
 	    
+	    /**
+	     * Returns the index of the column.
+	     * @return the index of the column.
+	     */
 	    public int getIndex() {
 	        return columnIndex;
 	    }
 	    
+	    /**
+	     * Returns the info gain.
+	     * @return the info gain.
+	     */
 	    public double getInfoGain() {
 	        return infoGain;
 	    }
 	    
+	    /**
+	     * Creates a string representation of the entropy data.
+	     * @return a string representation of the entropy data.
+	     */
 	    @Override
 	    public String toString() {
 	        return "Column Index: " + columnIndex + " - Info Gain: " + infoGain;
 	    }
 	}
 	
+	public double entropy(HashMap<Object, Integer> instanceCounts) {
+	    double entropy = 0;
+	    double log_2 = Math.log(2);
+        double totalInstances = 0;
+        for (Object i : instanceCounts.keySet()) {
+            totalInstances += instanceCounts.get(i);
+        }
+//	    System.out.println("Instance counts: " + instanceCounts + " Total Instances: " + totalInstances);
+        for (Object i : instanceCounts.keySet()) {
+            double ratio = ((double) instanceCounts.get(i)) / totalInstances;
+            entropy -=  (ratio) * (Math.log(ratio) / log_2);
+        }
+//	    System.out.println("Entropy: " + entropy);
+        return entropy;
+    }
+	
+	
+	
+	
 	
 	/**
+	 * Leaving this here for future reference (if needed)
 	 * @param index - for multiple target variables choose the index of the list for one
 	 * @return
 	 */
-	public void gain(int index) {
-		
-	    
-	    
+	public void loganGain(int index) {
 		// first keys are the values that are in the target column,
 		// second keys are column names
 		// third keys are unique vals in the column that point to count of occuances with a specific target variable
@@ -175,23 +237,6 @@ public class InformationGain {
 		System.out.println(column_e);
 	}
 	
-	public double entropy(HashMap<Object, Integer> instanceCounts) {
-	    double entropy = 0;
-	    double log_2 = Math.log(2);
-	    double totalInstances = 0;
-	    for (Object i : instanceCounts.keySet()) {
-	        totalInstances += instanceCounts.get(i);
-	    }
-	    System.out.println("Instance counts: " + instanceCounts + " Total Instances: " + totalInstances);
-	    for (Object i : instanceCounts.keySet()) {
-	        double ratio = ((double) instanceCounts.get(i)) / totalInstances;
-	        entropy -=  (ratio) * (Math.log(ratio) / log_2);
-	    }
-	    System.out.println("Entropy: " + entropy);
-	    return entropy;
-	}
-	
-	
 	public double entropy(Integer[] p) {
 		double e = 0;
 		int sum =0;
@@ -202,7 +247,6 @@ public class InformationGain {
 			e = e + (-1)* ((double)p[i]/sum) * ((Math.log10((double)p[i]/sum)) / Math.log10(2));
 		}
 		return e;
-		
 	}
 
 }
