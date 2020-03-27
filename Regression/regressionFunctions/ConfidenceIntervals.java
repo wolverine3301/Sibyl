3610b9129784fbc6cc37e14355dc4b7026412967
Gilgamesh69 <logancollier04@gmail.com>
2020-03-27 15:56:20 -0500

    stuff for confidence interval

--- Regression/regressionFunctions/ConfidenceIntervals.java
+++ Regression/regressionFunctions/ConfidenceIntervals.java
@@ -23,8 +23,9 @@ public class ConfidenceIntervals {
 	private float alpha;
 	private int confidence_level;
 	private float critical_prob;
-	private float[] SE_y; //must calculate standard error for all coefficients
-	
+	private float SE_y; 
+	private float[] SE_x; //must calculate standard error for all coefficients
+	private float[] T; 
 	public ConfidenceIntervals(Regression f) {
 		this.function = f;
 	}
@@ -35,13 +36,21 @@ public class ConfidenceIntervals {
 		this.critical_prob = 1 - (this.alpha/2);
 	}
 	private void setSE_y() {
-		for(int i = this.function.function.length-1; i >= 0 ;i--) {
-			this.SE_y[i] = (float) Math.sqrt((1/this.function.y.getLength()) + (this.function.SSE/(this.function.y.getLength()-2)));
-		}
+		this.SE_y = (float) Math.sqrt((1/this.function.y.getLength()) + (this.function.SSE/(this.function.y.getLength()-2)));
+
+	}
+	private void set_SE_coefficents() {
+		m = 
 	}
 	private void upper_intervalY(double predicted_y, double x_val) {
 		return predicted_y + 
 	}
+	private void set_Tvalue() {
+		for(int i = 0; i < this.function.function.length; i++) {
+			
+		}
+	}
+
 	
 
 }
