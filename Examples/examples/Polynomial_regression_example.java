6cdc69ec30cc12c0baa79b73b221a014eebe494e
Gilgamesh69 <logancollier04@gmail.com>
2020-03-31 16:54:17 -0500

    rerw

--- Examples/examples/Polynomial_regression_example.java
+++ Examples/examples/Polynomial_regression_example.java
@@ -6,12 +6,20 @@ import regressionFunctions.PolyRegression;
 public class Polynomial_regression_example {
 
 	public static void main(String[] args) {
-		double[] a1 = {-1,0,1,2,3,5,7,9};
-		double[] a2 = {-1,3,2.5,5,4,2,5,4};
+		double[] a1 = {0,2,2,5,5,9,9,9,9,10};
+		double[] a2 = {-2,0,2,1,3,1,0,0,1,-1};
 		Column x = new Column(a1);
 		Column y = new Column(a2);
-		PolyRegression p = new PolyRegression(x,y,4);
+		PolyRegression p = new PolyRegression(x,y,2);
 		p.printEquation();
+		System.out.println("MSE: "+p.MSE);
+		System.out.println("R2: "+p.R2);
+		System.out.println("RMSD: "+p.RMSD);
+
+		//ystem.out.println(p.SE);
+		p.get_T_coeffiecents();
+		//p.get_SE_coeffiecents();
+		
 	}
 
 }
