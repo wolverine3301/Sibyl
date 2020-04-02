6bb1c38008c6347538876a0914b39b01051dded6
Gilgamesh69 <logancollier04@gmail.com>
2020-04-02 15:15:48 -0700

    STUFF

--- Examples/examples/Polynomial_regression_example.java
+++ Examples/examples/Polynomial_regression_example.java
@@ -10,7 +10,7 @@ public class Polynomial_regression_example {
 		double[] a2 = {-2,0,2,1,3,1,0,0,1,-1};
 		Column x = new Column(a1);
 		Column y = new Column(a2);
-		PolyRegression p = new PolyRegression(x,y,2);
+		PolyRegression p = new PolyRegression(x,y,1);
 		p.printEquation();
 		System.out.println("MSE: "+p.MSE);
 		System.out.println("R2: "+p.R2);
