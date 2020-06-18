7a9e8e6661f4503c70b1264ebf68851951e8c735
cadereynoldson <cader444@gmail.com>
2020-06-18 15:47:26 -0700

    sychronized iterators

---
+++ Saga/dataframe_utilities/Synchronized_RowIterator.java
@@ -0,0 +1,25 @@
+package dataframe_utilities;
+
+import java.util.Iterator;
+
+import dataframe.DataFrame;
+
+public class Synchronized_RowIterator implements Iterator<DataFrame_Iterable> {
+
+    private DataFrame dataFrame;
+    
+    private int currentColumn;
+    
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public DataFrame_Iterable next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
